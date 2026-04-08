3d8c99aacbc47831bfc2186e39490daebab577da
madhaviporte <madhaviporte2004@gmail.com>
2026-04-08 19:31:03 +0530

    login and register

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,9 +1,17 @@
 import {createBrowserRouter} from 'react-router'
 import Login from './features/auth/pages/Login';
 import Register from './features/auth/pages/Register';
+// import Home from './pages/Home'
+
 
 
 export const router = createBrowserRouter([
+
+//     {
+//   path:'/',
+//   element: <Home/>
+//     },
+
     {
         path: '/login',
         element: <Login/>
