993cbe87959d5c790fc561af5539cb4e015e9107
madhaviporte <madhaviporte2004@gmail.com>
2026-06-23 23:16:05 +0530

    backend part

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,40 +1,43 @@
-const jwt = require('jsonwebtoken')
-const tokenBlacklistModel = require('../models/blacklist.model')
+const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
 
 
 
-async function authUser(req,res,next){
-    
+async function authUser(req, res, next) {
+
     const token = req.cookies.token
 
-    if(!token){
+    if (!token) {
         return res.status(401).json({
-            message:'Token not Provided.'
+            message: "Token not provided."
         })
     }
 
     const isTokenBlacklisted = await tokenBlacklistModel.findOne({
-        token 
+        token
     })
 
-    if(isTokenBlacklisted){
+    if (isTokenBlacklisted) {
         return res.status(401).json({
-            message:'Token is Invalid'
+            message: "token is invalid"
         })
     }
 
-    try{
-  const decoded=  jwt.verify(token,process.env.JWT_SECRET)
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-  req.user = decoded
+        req.user = decoded
 
-  next()
-}
-catch(err){
-    return res.status(401).json({
-        message:"Invalid token."
-    })
-}
+        next()
+
+    } catch (err) {
+
+        return res.status(401).json({
+            message: "Invalid token."
+        })
+    }
+
 }
 
-module.exports = {authUser}
+
+module.exports = { authUser }
