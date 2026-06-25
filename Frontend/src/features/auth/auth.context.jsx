7303b0f1d735e8898c5f04ffa8fb8d97b3910064
madhaviporte <madhaviporte2004@gmail.com>
2026-06-25 17:42:35 +0530

    Frontend reedit

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -9,14 +9,10 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    
-
-
     return (
         <AuthContext.Provider value={{user,setUser,loading,setLoading}} >
             {children}
         </AuthContext.Provider>
     )
-
     
 }
