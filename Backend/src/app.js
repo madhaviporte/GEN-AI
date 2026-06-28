3110a76cda72f0297bb9db01c5e2dc72534a076c
madhaviporte <madhaviporte2004@gmail.com>
2026-06-28 23:04:10 +0530

    origin chnage after frontend deploy in bakend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://gen-ai-1-frontend-g7vo.onrender.com",
     credentials: true
 }))
 
